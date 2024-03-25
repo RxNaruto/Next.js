c4f02a17a2b13ae24402e90fbd34507ebce01468
Rithk <rithkchaudhary86@gmail.com>
2024-03-25 23:45:18 +0530

    back to work

---
+++ backend3/app/page.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+async function getUserDetail(){
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+  return response.data;
+}
+
+export default async function Home(){
+  const userdata=await getUserDetail();
+
+  return(
+    <div>
+     {userdata.email}
+      <br />
+     {userdata.name}
+    </div>
+  )
+}

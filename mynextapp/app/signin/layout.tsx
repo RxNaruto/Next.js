29a70fefda0735c75baf9dd9f748b3b8c6fef851
Rithk <rithkchaudhary86@gmail.com>
2024-06-08 18:11:46 +0530

    layout managing

---
+++ mynextapp/app/signin/layout.tsx
@@ -0,0 +1,11 @@
+export default function({children}: {children: 
+    React.ReactNode
+}){
+    return <div>
+        <div className="p-4 border-b">
+        20% off on choosing advance plan
+
+    </div>
+    {children}
+    </div>
+}

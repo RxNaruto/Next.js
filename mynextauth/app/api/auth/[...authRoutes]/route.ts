31c34f690109aa5b1686cb80e33dce338adbac24
Rithk <rithkchaudhary86@gmail.com>
2024-06-10 16:24:42 +0530

    catching routes

---
+++ mynextauth/app/api/auth/[...authRoutes]/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(req: NextRequest, arg : any){
+    console.log(arg.params.authRoutes);
+    return NextResponse.json({
+        message: "hello word"
+    })
+} 
